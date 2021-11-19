3a4d4d8a785ecff8bb924ff9f096b3e765201c07
Eric-157 <eedwards445566@gmail.com>
2021-11-19 13:45:00 -0500

    Last few changes
    Fixed bugs, added new media queries and finalized what we could before sending it off.

--- WebsiteAttempt2/javascript.js
+++ WebsiteAttempt2/javascript.js
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////
+//  NWTF Website                                //
+//                                              //
+//  Javascript file for Menu                    //
+//                                              //
+//  Authors: Ryan Griffin and Eric Edwards      //
+//                                              //
+//  Date: 11/19/2021                            //
+//////////////////////////////////////////////////
+
+// Button for mobile navigation drop down menu//
 function button_press() {
     var x = document.getElementById("navlinks");
     var y = document.getElementById("button")
