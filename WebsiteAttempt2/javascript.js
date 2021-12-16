c9a599826b6f97f64213b93e26544eee733e5904
ryan-griffin <ryan-griffin@users.noreply.github.com>
2021-12-16 11:07:27 -0500

    update

--- WebsiteAttempt2/javascript.js
+++ WebsiteAttempt2/javascript.js
@@ -10,15 +10,5 @@
 
 // Button for mobile navigation drop down menu//
 function button_press() {
-    var x = document.getElementById("navlinks");
-    var y = document.getElementById("button")
-    if (x.style.maxHeight === "432px") {
-      x.style.maxHeight = "0px";
-      x.style.boxShadow = "none";
-      y.style.backgroundImage = "url(buttons/hamburger.png";
-    } else {
-      x.style.maxHeight = "432px";
-      x.style.boxShadow = "0px 0px 10px 5px rgba(0,0,0,0.3)";
-      y.style.backgroundImage = "url(buttons/close.png";
-    }
+  document.querySelector("nav").classList.toggle("open");
 }
